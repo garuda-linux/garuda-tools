ab2f92061328c5fa2705c647242e94e59fc923c2
Nico Jensch <njcrypted@protonmail.com>
2020-09-16 15:41:55 +0200

    Add telegram notifications after building

--- lib/util-iso.sh
+++ lib/util-iso.sh
@@ -219,6 +219,8 @@ make_iso() {
     ${torrent} && make_torrent
     ${checksum} && checksumiso "${iso_dir}"
 
+	echo "${iso_file} built successfully!" | apprise -vv "tgram://1368184718:AAHTO7Pq_Uvm9HCBeYjTZzcMZ-3huZbtrm4/-1001325273433" -t "New ISO available!"
+
     msg "Done [Build ISO]"
 }
 
