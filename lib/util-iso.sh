c960bc951c58df13db795f3459a318d21c017e46
TNE <tne@garudalinux.org>
2022-09-04 19:17:09 +0000

    ibt=off

--- lib/util-iso.sh
+++ lib/util-iso.sh
@@ -517,7 +517,7 @@ make_image_boot() {
 
 configure_grub(){
     local default_args="misobasedir=${iso_name} misolabel=${iso_label}" \
-        boot_args=('quiet' 'systemd.show_status=1' ${custom_boot_args} ${apparmor_boot_args})
+        boot_args=('quiet' 'systemd.show_status=1' 'ibt=off' ${custom_boot_args} ${apparmor_boot_args})
 
     sed -e "s|@DIST_NAME@|${dist_name}|g" \
         -e "s|@ARCH@|${target_arch}|g" \
